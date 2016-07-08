c39f0d67a2c23b849aa6197aafec8eee9205a33b
Donovan <donovan.nutting@hgs.hiddengeniusproject.rog>
2016-07-08 11:26:45 -0700

    First App

--- hiddenGeniusProject/ViewController.swift
+++ hiddenGeniusProject/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Nutt(sender: AnyObject)
+    {
+        peanutNut.hidden = true
+    }
+    @IBOutlet weak var peanutNut: UIImageView!
+    @IBOutlet weak var peanut1: UIImageView!
+
+    @IBOutlet weak var peanut3: UIImageView!
+    @IBOutlet weak var peanut2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
